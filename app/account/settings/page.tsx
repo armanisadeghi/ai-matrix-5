c756662609f9b41435f9c3a0f8dcab87a5dcc7d7
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2024-05-25 17:45:53 +0300

    feat: added accounts integration page

--- app/account/settings/page.tsx
+++ app/account/settings/page.tsx
@@ -1,72 +1,57 @@
 "use client";
 
 import { Box, rem, Tabs, Title } from "@mantine/core";
-import { IconMoodCog, IconSettings, IconUserCircle } from "@tabler/icons-react";
+import { IconAffiliate, IconMoodCog, IconSettings, IconUserCircle } from "@tabler/icons-react";
 import PersonalInformation from "@/app/account/settings/tabs/PersonalInformation";
 import Preferences from "@/app/account/settings/tabs/Preferences";
+import Integrations from "@/app/account/settings/tabs/Integrations";
 
 const iconStyle = { width: rem(14), height: rem(14) };
 
 const SettingsPage = () => {
-  return (
-    <>
-      <Box>
-        <Title order={3} mb="md">
-          Clients
-        </Title>
-        <Tabs defaultValue="personal">
-          <Tabs.List mb="lg">
-            <Tabs.Tab
-              value="personal"
-              leftSection={<IconUserCircle style={iconStyle} />}
-            >
-              Personal Information
-            </Tabs.Tab>
-            <Tabs.Tab
-              value="preferences"
-              leftSection={<IconMoodCog style={iconStyle} />}
-            >
-              Preferences
-            </Tabs.Tab>
-            <Tabs.Tab
-              value="connections"
-              leftSection={<IconSettings style={iconStyle} />}
-            >
-              Connections
-            </Tabs.Tab>
-            <Tabs.Tab
-              value="billing"
-              leftSection={<IconSettings style={iconStyle} />}
-            >
-              Billing
-            </Tabs.Tab>
-            <Tabs.Tab
-              value="security"
-              leftSection={<IconSettings style={iconStyle} />}
-            >
-              Security
-            </Tabs.Tab>
-            <Tabs.Tab
-              value="notifications"
-              leftSection={<IconSettings style={iconStyle} />}
-            >
-              Notifications
-            </Tabs.Tab>
-          </Tabs.List>
+    return (
+        <>
+            <Box>
+                <Title order={3} mb="md">
+                    Clients
+                </Title>
+                <Tabs defaultValue="personal">
+                    <Tabs.List mb="lg">
+                        <Tabs.Tab value="personal" leftSection={<IconUserCircle style={iconStyle} />}>
+                            Personal Information
+                        </Tabs.Tab>
+                        <Tabs.Tab value="preferences" leftSection={<IconMoodCog style={iconStyle} />}>
+                            Preferences
+                        </Tabs.Tab>
+                        <Tabs.Tab value="integrations" leftSection={<IconAffiliate style={iconStyle} />}>
+                            Integrations
+                        </Tabs.Tab>
+                        <Tabs.Tab value="billing" leftSection={<IconSettings style={iconStyle} />}>
+                            Billing
+                        </Tabs.Tab>
+                        <Tabs.Tab value="security" leftSection={<IconSettings style={iconStyle} />}>
+                            Security
+                        </Tabs.Tab>
+                        <Tabs.Tab value="notifications" leftSection={<IconSettings style={iconStyle} />}>
+                            Notifications
+                        </Tabs.Tab>
+                    </Tabs.List>
 
-          <Tabs.Panel value="personal">
-            <PersonalInformation />
-          </Tabs.Panel>
+                    <Tabs.Panel value="personal">
+                        <PersonalInformation />
+                    </Tabs.Panel>
 
-          <Tabs.Panel value="preferences">
-            <Preferences />
-          </Tabs.Panel>
+                    <Tabs.Panel value="preferences">
+                        <Preferences />
+                    </Tabs.Panel>
 
-          <Tabs.Panel value="settings">Settings tab content</Tabs.Panel>
-        </Tabs>
-      </Box>
-    </>
-  );
+                    <Tabs.Panel value="integrations">
+                        <Integrations />
+                    </Tabs.Panel>
+                </Tabs>
+            </Box>
+        </>
+    );
 };
 
 export default SettingsPage;
