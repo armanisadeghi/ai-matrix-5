5d5e93418978dd8166e65187193972c20e2eb9a0
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2024-05-27 21:14:59 +0300

    feat(ui): added aside popup for chats on MainLayout

--- layout/Main/index.tsx
+++ layout/Main/index.tsx
@@ -1,11 +1,23 @@
 "use client";
 
-import { AppShell, Breadcrumbs, rem, Text, useMantineTheme } from "@mantine/core";
+import {
+    ActionIcon,
+    Affix,
+    AppShell,
+    Breadcrumbs,
+    CloseButton,
+    rem,
+    Text,
+    Title,
+    Tooltip,
+    useMantineTheme,
+} from "@mantine/core";
 import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import { useState, useEffect, ReactNode } from "react";
 import { Navbar } from "@/layout/Main/Navbar";
 import { Header } from "@/layout/Main/Header";
 import Link from "next/link";
+import { IconBubbleText } from "@tabler/icons-react";
 
 // sample breadcrumbs to help with navigation
 const items = [
@@ -32,6 +44,7 @@ export function MainLayout(props: Props) {
     const mobile_match = useMediaQuery("(min-width: 768px)");
     const theme = useMantineTheme();
     const [isCollapsed, setIsCollapsed] = useState(defaultNavCollapse);
+    const [isChatOpen, setIsChatOpen] = useState(false);
 
     useEffect(() => {
         if (forceBaseNavbar) {
@@ -42,51 +55,79 @@ export function MainLayout(props: Props) {
     }, [forceBaseNavbar, desktop_match, tablet_match]);
 
     return (
-        <AppShell
-            header={{ height: { base: 60, md: 60, lg: 70 } }}
-            navbar={{
-                width: { base: 60, md: 200, lg: 250 },
-                breakpoint: "sm",
-                collapsed: { mobile: !opened },
-            }}
-            padding="sm"
-        >
-            <AppShell.Header>
-                <Header
-                    opened={opened}
-                    toggle={toggle}
-                    desktopOpened={isCollapsed}
-                    toggleDesktop={() => setIsCollapsed(!isCollapsed)}
-                    tabletMatch={mobile_match}
-                />
-            </AppShell.Header>
-            <AppShell.Navbar
-                pt="md"
-                style={{
-                    width: isCollapsed ? "60px" : undefined,
-                    transition: "width 0.3s",
-                    paddingLeft: isCollapsed ? 0 : theme.spacing.xs,
-                    paddingRight: isCollapsed ? 0 : theme.spacing.xs,
+        <>
+            <AppShell
+                header={{ height: { base: 60, md: 60, lg: 70 } }}
+                navbar={{
+                    width: { base: 60, md: 200, lg: 250 },
+                    breakpoint: "sm",
+                    collapsed: { mobile: !opened },
                 }}
-            >
-                <Navbar
-                    desktopOpened={desktop_match && !forceBaseNavbar}
-                    tabletOpened={tablet_match && !forceBaseNavbar}
-                    mobileOpened={mobile_match && !forceBaseNavbar}
-                    isCollapsed={isCollapsed}
-                />
-            </AppShell.Navbar>
-            <AppShell.Main
-                // quick hack
-                style={{
-                    paddingInlineStart: isCollapsed
-                        ? rem(80)
-                        : `calc(var(--app-shell-navbar-offset, 0rem) + var(--app-shell-padding)`,
+                aside={{
+                    width: isChatOpen ? 300 : 0,
+                    breakpoint: "sm",
                 }}
+                padding="sm"
             >
-                <Breadcrumbs mb="sm">{items}</Breadcrumbs>
-                {children}
-            </AppShell.Main>
-        </AppShell>
+                <AppShell.Header>
+                    <Header
+                        opened={opened}
+                        toggle={toggle}
+                        desktopOpened={isCollapsed}
+                        toggleDesktop={() => setIsCollapsed(!isCollapsed)}
+                        tabletMatch={mobile_match}
+                    />
+                </AppShell.Header>
+                <AppShell.Navbar
+                    pt="md"
+                    style={{
+                        width: isCollapsed ? "60px" : undefined,
+                        transition: "width 0.3s",
+                        paddingLeft: isCollapsed ? 0 : theme.spacing.xs,
+                        paddingRight: isCollapsed ? 0 : theme.spacing.xs,
+                    }}
+                >
+                    <Navbar
+                        desktopOpened={desktop_match && !forceBaseNavbar}
+                        tabletOpened={tablet_match && !forceBaseNavbar}
+                        mobileOpened={mobile_match && !forceBaseNavbar}
+                        isCollapsed={isCollapsed}
+                    />
+                </AppShell.Navbar>
+                <AppShell.Main
+                    // quick hack
+                    style={{
+                        paddingInlineStart: isCollapsed
+                            ? rem(80)
+                            : `calc(var(--app-shell-navbar-offset, 0rem) + var(--app-shell-padding)`,
+                    }}
+                >
+                    <Breadcrumbs mb="sm">{items}</Breadcrumbs>
+                    {children}
+                </AppShell.Main>
+                {isChatOpen && (
+                    <AppShell.Aside p="md">
+                        <CloseButton onClick={() => setIsChatOpen(false)} />
+                        <Title order={5}>You can use this to show chats</Title>
+                    </AppShell.Aside>
+                )}
+            </AppShell>
+            {!isChatOpen && (
+                <Affix position={{ bottom: 20, right: 20 }}>
+                    <Tooltip label="Open chat">
+                        <ActionIcon
+                            size={tablet_match ? "lg" : 48}
+                            color="blue"
+                            variant="filled"
+                            radius="50%"
+                            onClick={() => setIsChatOpen(!isChatOpen)}
+                            style={{ boxShadow: theme.shadows.xl }}
+                        >
+                            <IconBubbleText />
+                        </ActionIcon>
+                    </Tooltip>
+                </Affix>
+            )}
+        </>
     );
 }
