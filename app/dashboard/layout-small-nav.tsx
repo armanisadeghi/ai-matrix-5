a32715d2df4918720ac679172588ae25a4a32e35
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2024-05-28 17:40:32 +0300

    feat(ui): updated dashboard/intelligence layout

--- app/dashboard/layout-small-nav.tsx
+++ app/dashboard/layout-small-nav.tsx
@@ -1,18 +1,14 @@
 // app/dashboard/layout-small-nav.tsx
 
-import { ReactNode } from 'react';
-import { AppsLayout } from '@/layout';
+import { ReactNode } from "react";
+import { AppsLayout, MainLayout } from "@/layout";
 
 type Props = {
     children: ReactNode;
 };
 
 function SmallSidebarLayout({ children }: Props) {
-    return (
-        <AppsLayout>
-            {children}
-        </AppsLayout>
-    );
+    return <MainLayout defaultNavCollapse={true}>{children}</MainLayout>;
 }
 
 export default SmallSidebarLayout;
