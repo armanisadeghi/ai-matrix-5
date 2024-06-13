d690c536b29eb53cd72fb83c28c3bd522f94c191
kelvin <kelvin.kiprop96@gmail.com>
2024-06-14 00:26:42 +0300

    fix: added function to generate unique navbar keys

--- layout/Main/Navbar/Navbar.tsx
+++ layout/Main/Navbar/Navbar.tsx
@@ -1,14 +1,5 @@
 "use client";
-import {
-    ActionIcon,
-    ActionIconProps,
-    AppShell,
-    Group,
-    Menu,
-    NavLink,
-    ScrollArea,
-    useMantineTheme,
-} from "@mantine/core";
+import { ActionIconProps, AppShell, Flex, Group, Menu, NavLink, ScrollArea, useMantineTheme } from "@mantine/core";
 import {
     IconChevronLeft,
     IconChevronRight,
@@ -19,11 +10,12 @@ import {
     IconSettings,
     IconShield,
 } from "@tabler/icons-react";
-import {navItems} from "./navItems";
-import {usePathname} from "next/navigation";
+import { navItems } from "./navItems";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import AmeActionIcon from "@/ui/buttons/AmeActionIcon";
-import {useNavbar} from "@/context/NavbarContext";
+import { useNavbar } from "@/context/NavbarContext";
+import { generateKey } from "@/utils/keys";
 
 const actionProps: ActionIconProps = {
     variant: "light",
@@ -33,21 +25,21 @@ interface NavbarProps {
     state: "full" | "compact" | "icons" | "hidden";
 }
 
-export const Navbar = ({state}: NavbarProps) => {
-    const {handleNavbarExpand, handleNavbarCollapse} = useNavbar();
+export const Navbar = ({ state }: NavbarProps) => {
+    const { handleNavbarExpand, handleNavbarCollapse } = useNavbar();
     const theme = useMantineTheme();
     const pathname = usePathname();
 
     return (
         <>
-            <AppShell.Section>
+            <AppShell.Section pt="xs">
                 {state === "compact" && (
                     <Group justify="flex-end" gap="xs">
                         <AmeActionIcon title="shrink navbar" onClick={handleNavbarCollapse} {...actionProps}>
-                            <IconChevronLeft size={18}/>
+                            <IconChevronLeft size={18} />
                         </AmeActionIcon>
                         <AmeActionIcon title="expand navbar" onClick={handleNavbarExpand} {...actionProps}>
-                            <IconChevronRight size={18}/>
+                            <IconChevronRight size={18} />
                         </AmeActionIcon>
                     </Group>
                 )}
@@ -55,117 +47,115 @@ export const Navbar = ({state}: NavbarProps) => {
                 {state === "full" && (
                     <Group justify="flex-end" gap="xs">
                         <AmeActionIcon title="shrink navbar" onClick={handleNavbarCollapse} {...actionProps}>
-                            <IconChevronLeft size={18}/>
+                            <IconChevronLeft size={18} />
                         </AmeActionIcon>
                     </Group>
                 )}
                 {state === "icons" && (
                     <Group justify="center" gap="xs">
                         <AmeActionIcon title="shrink navbar" onClick={handleNavbarExpand} {...actionProps}>
-                            <IconChevronRight size={18}/>
+                            <IconChevronRight size={18} />
                         </AmeActionIcon>
                     </Group>
                 )}
             </AppShell.Section>
 
             <AppShell.Section grow component={ScrollArea} my="md">
-                {navItems.map((item, index) => (
-                    <>
-                        <NavLink
-                            key={item.label + index + window.performance.now()}
-                            label={
-                                <Group
-                                    style={{whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}}
-                                    gap="xs"
-                                >
-                                    <item.icon size={20}/>
-                                    {state !== "icons" && (
-                                        <span
-                                            style={{
-                                                marginLeft: "8px",
-                                                flex: "1",
-                                                overflow: "hidden",
-                                                textOverflow: "ellipsis",
-                                            }}
-                                        >
-                                            {item.label}
-                                        </span>
-                                    )}
-                                </Group>
-                            }
-                            childrenOffset={28}
-                            defaultOpened={item.initiallyOpened}
-                            mb="xs"
-                            style={{borderRadius: theme.radius.sm}}
-                        >
-                            {state !== "icons" &&
-                                item.links.map((link, linkIndex) => (
-                                    <NavLink
-                                        key={item.label + link.label + window.performance.now()}
-                                        label={link.label}
-                                        component={Link}
-                                        href={link.link}
-                                        mt={linkIndex === 0 ? 4 : 0}
+                {navItems.map((item) => (
+                    <NavLink
+                        key={generateKey(item.label)}
+                        label={
+                            <Group
+                                style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}
+                                gap="xs"
+                                key={generateKey(item.label + "group")}
+                            >
+                                <item.icon size={20} key={generateKey(item.label + "icon")} />
+                                {state !== "icons" && (
+                                    <span
                                         style={{
-                                            borderRadius: theme.radius.sm,
-                                            backgroundColor: link.link === pathname ? theme.colors.gray[3] : "inherit",
-                                            color: link.link === pathname ? theme.colors.dark[8] : "inherit",
-                                            fontWeight: link.link === pathname ? 600 : "normal",
+                                            marginLeft: "8px",
+                                            flex: "1",
+                                            overflow: "hidden",
+                                            textOverflow: "ellipsis",
                                         }}
-                                    />
-                                ))}
-                        </NavLink>
-                    </>
+                                    >
+                                        {item.label}
+                                    </span>
+                                )}
+                            </Group>
+                        }
+                        childrenOffset={28}
+                        defaultOpened={item.initiallyOpened}
+                        mb="xs"
+                        style={{ borderRadius: theme.radius.sm }}
+                    >
+                        {state !== "icons" &&
+                            item.links.map((link, linkIndex) => (
+                                <NavLink
+                                    label={link.label}
+                                    component={Link}
+                                    href={link.link}
+                                    mt={linkIndex === 0 ? 4 : 0}
+                                    style={{
+                                        borderRadius: theme.radius.sm,
+                                        backgroundColor: link.link === pathname ? theme.colors.gray[3] : "inherit",
+                                        color: link.link === pathname ? theme.colors.dark[8] : "inherit",
+                                        fontWeight: link.link === pathname ? 600 : "normal",
+                                    }}
+                                />
+                            ))}
+                    </NavLink>
                 ))}
             </AppShell.Section>
 
-            <AppShell.Section>
-                <Group justify="center" gap="md">
+            <AppShell.Section pb="xs">
+                <Flex justify="center" gap="xs">
                     {state !== "icons" ? (
                         <>
-                            <ActionIcon variant="transparent" size="sm">
-                                <IconSettings/>
-                            </ActionIcon>
-                            <ActionIcon variant="transparent" size="sm">
-                                <IconMessage/>
-                            </ActionIcon>
-                            <ActionIcon variant="transparent" size="sm">
-                                <IconHelp/>
-                            </ActionIcon>
-                            <ActionIcon variant="transparent" size="sm">
-                                <IconFile/>
-                            </ActionIcon>
-                            <ActionIcon variant="transparent" size="sm">
-                                <IconShield/>
-                            </ActionIcon>
+                            <AmeActionIcon title="settings" variant="transparent">
+                                <IconSettings />
+                            </AmeActionIcon>
+                            <AmeActionIcon title="messages" variant="transparent">
+                                <IconMessage />
+                            </AmeActionIcon>
+                            <AmeActionIcon title="help" variant="transparent">
+                                <IconHelp />
+                            </AmeActionIcon>
+                            <AmeActionIcon title="files" variant="transparent">
+                                <IconFile />
+                            </AmeActionIcon>
+                            <AmeActionIcon title="locks" variant="transparent">
+                                <IconShield />
+                            </AmeActionIcon>
                         </>
                     ) : (
                         <Menu>
                             <Menu.Target>
-                                <ActionIcon>
-                                    <IconDots/>
-                                </ActionIcon>
+                                <AmeActionIcon title="menu" variant="transparent">
+                                    <IconDots />
+                                </AmeActionIcon>
                             </Menu.Target>
                             <Menu.Dropdown>
                                 <Menu.Item>
-                                    <IconSettings/>
+                                    <IconSettings />
                                 </Menu.Item>
                                 <Menu.Item>
-                                    <IconMessage/>
+                                    <IconMessage />
                                 </Menu.Item>
                                 <Menu.Item>
-                                    <IconHelp/>
+                                    <IconHelp />
                                 </Menu.Item>
                                 <Menu.Item>
-                                    <IconFile/>
+                                    <IconFile />
                                 </Menu.Item>
                                 <Menu.Item>
-                                    <IconShield/>
+                                    <IconShield />
                                 </Menu.Item>
                             </Menu.Dropdown>
                         </Menu>
                     )}
-                </Group>
+                </Flex>
             </AppShell.Section>
         </>
     );
