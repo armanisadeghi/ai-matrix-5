7dc34e12ce3c0feb974e687b9faa49b90b76ca7f
Superstar823 <superstardev823@gmail.com>
2024-06-13 11:08:15 +0900

    fix the middleware for auth0

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,49 @@
-import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export default withMiddlewareAuthRequired();
+export function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname.startsWith('/samples/sample-login')) {
+        return NextResponse.rewrite(new URL('/api/auth/login', request.url))
+    }
+
+    if (request.nextUrl.pathname.startsWith('/dashboard/sample-login')) {
+        return NextResponse.rewrite(new URL('/api/auth/login', request.url))
+    }
+
+    if (request.nextUrl.pathname.startsWith('/matrix-engine')) {
+        return NextResponse.rewrite(new URL('/api/auth/login', request.url))
+    }
+}
 
 export const config = {
-  matcher: ["/samples", "/dashboard"],
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        {
+            source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+            missing: [
+                { type: 'header', key: 'next-router-prefetch' },
+                { type: 'header', key: 'purpose', value: 'prefetch' },
+            ],
+        },
+
+        {
+            source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+            has: [
+                { type: 'header', key: 'next-router-prefetch' },
+                { type: 'header', key: 'purpose', value: 'prefetch' },
+            ],
+        },
+
+        {
+            source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+            has: [{ type: 'header', key: 'x-present' }],
+            missing: [{ type: 'header', key: 'x-missing', value: 'prefetch' }],
+        },
+    ],
 };
