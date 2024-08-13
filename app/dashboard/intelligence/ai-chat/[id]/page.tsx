2f472ffb55539342de802bb70acfa10796ab27bb
kelvin <kelvin.kiprop96@gmail.com>
2024-08-13 23:50:04 +0300

    [kelvin] feat: updated chat header title

--- app/dashboard/intelligence/ai-chat/[id]/page.tsx
+++ app/dashboard/intelligence/ai-chat/[id]/page.tsx
@@ -1,23 +1,16 @@
 // app/dashboard/intelligence/ai-chat/[id]/page.tsx
-
-import { ResponseArea } from '@/components/AiChat';
-import AutoFetch from '@/components/AiChat/new/AutoFetch';
-import React from 'react';
-import ChatClientLogic from './ChatClientLogic';
-
+import ChatClientLogic from "./ChatClientLogic";
 
 interface PageProps {
     params: { id: string };
 }
 
-export default function Page({params}: PageProps) {
+export default function Page({ params }: PageProps) {
     const chatId = decodeURIComponent(params.id);
 
     return (
         <>
-
-
-            <ChatClientLogic chatId={chatId}/>
+            <ChatClientLogic chatId={chatId} />
         </>
     );
 }
--- app/dashboard/intelligence/ai-chat/layout.tsx
+++ app/dashboard/intelligence/ai-chat/layout.tsx
@@ -1,10 +1,15 @@
-import { ResponseArea } from '@/components/AiChat';
-import AutoFetch from '@/components/AiChat/new/AutoFetch';
-import ChatSidebar from '@/components/AiChat/Sidebar/ChatSidebar';
-import React, { ReactNode, useMemo } from 'react';
-import UserInputArea from '@/app/dashboard/intelligence/ai-chat/components/UserInputArea';
-import CenterLayout from '@/ui/layout/AmeCenterContent/CenterLayout';
+"use client";
 
+import { Button, Group, Text } from "@mantine/core";
+import { useSearchParams } from "next/navigation";
+import React, { ReactNode, useMemo } from "react";
+
+import UserInputArea from "@/app/dashboard/intelligence/ai-chat/components/UserInputArea";
+import { ResponseArea } from "@/components/AiChat";
+import AutoFetch from "@/components/AiChat/new/AutoFetch";
+import ChatSidebar from "@/components/AiChat/Sidebar/ChatSidebar";
+import CenterLayout from "@/ui/layout/AmeCenterContent/CenterLayout";
+import { IconExclamationCircle } from "@tabler/icons-react";
 
 type Props = {
     children: ReactNode;
@@ -14,25 +19,33 @@ const MemoizedUserInputArea = React.memo(UserInputArea);
 const MemoizedChatSidebar = React.memo(ChatSidebar);
 const MemoizedCenterLayout = React.memo(CenterLayout);
 
-export default function Layout({children}: Props) {
+export default function Layout({ children }: Props) {
+    const searchParams = useSearchParams();
+    let chatTitle = searchParams.get("chatTitle") ?? "New chat";
 
-    const memoizedLayout = useMemo(() => (
-        <>
-            <MemoizedCenterLayout
-                bottomSectionContent={<MemoizedUserInputArea/>}
-                centerSectionWidth={83}
-                rightMenuSectionContent={<MemoizedChatSidebar/>}
-                rightMenuSectionWidth={17}
-                scrollAreaContent={<ResponseArea className="responseTextArea" bottomPadding={0}/>}
-            />
-            {children}
-        </>
-    ), [children]);
-
-    return (
-        <AutoFetch>
-            {memoizedLayout}
-        </AutoFetch>
+    const memoizedLayout = useMemo(
+        () => (
+            <>
+                <MemoizedCenterLayout
+                    bottomSectionContent={<MemoizedUserInputArea />}
+                    centerSectionWidth={83}
+                    rightMenuSectionContent={<MemoizedChatSidebar />}
+                    rightMenuSectionWidth={17}
+                    scrollAreaContent={<ResponseArea className="responseTextArea" bottomPadding={0} />}
+                    topSectionContent={
+                        <Group justify="space-between" py="xs">
+                            <Text>{chatTitle}</Text>
+                            <Button variant="subtle" leftSection={<IconExclamationCircle size={16} />}>
+                                Guided Tour
+                            </Button>
+                        </Group>
+                    }
+                />
+                {children}
+            </>
+        ),
+        [children],
     );
-}
 
+    return <AutoFetch>{memoizedLayout}</AutoFetch>;
+}
