005b66d62d9df4d77af9063242fc84b2282f91bc
Kelvin Kiprop <kelvin.kiprop96@gmail.com>
2024-09-28 18:27:02 +0300

    [kelvin] chore: cleaned up file /folder structure view

---
+++ app/dashboard/code-editor/components/FolderStructure/TreeNode.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { IconDotsVertical, IconFolderFilled, IconFolderOpen, IconPencil, IconTrash } from "@tabler/icons-react";
+import { Menu } from "@mantine/core";
+
+import { useEffect, useState } from "react";
+import { getIconFromExtension, indexedDBStore } from "../../utils";
+import { IFile } from "../../workspace/[repoName]/page";
+import { IFileNode } from "./utils";
+
+type TreeNodeProps = React.HTMLAttributes<HTMLDivElement> & {
+    node: IFileNode;
+    path: string;
+    repoName: string;
+    onFileSelect?: (path: string, content: string) => void;
+    onFolderSelect: (path: string) => void;
+    onUpdate: () => void;
+    activeFolder: string;
+    selectedFile: IFile;
+};
+
+export const TreeNode: React.FC<TreeNodeProps> = ({
+    node,
+    path,
+    repoName,
+    onFileSelect,
+    onFolderSelect,
+    onUpdate,
+    activeFolder,
+    selectedFile,
+    ...others
+}) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [isRenaming, setIsRenaming] = useState(false);
+    const [newName, setNewName] = useState(node.name);
+    const fullPath = path ? `${path}/${node.name}` : node.name;
+    const isActive = fullPath === activeFolder;
+
+    const handleFolderClick = () => {
+        setIsExpanded(!isExpanded);
+        onFolderSelect(fullPath);
+    };
+
+    const decodeBase64 = (content: string) => atob(content);
+
+    const FileIcon = node.isFolder ? (isExpanded ? IconFolderOpen : IconFolderFilled) : getIconFromExtension(node.name);
+
+    const handleRename = async () => {
+        if (isRenaming) {
+            const oldPath = fullPath;
+            const newPath = path ? `${path}/${newName}` : newName;
+
+            try {
+                if (node.isFolder) {
+                    await indexedDBStore.updateFolder(repoName, oldPath, newPath);
+                } else {
+                    const content = node.content ? decodeBase64(node.content) : "";
+                    await indexedDBStore.updateFile(repoName, oldPath, newPath, content);
+                }
+                onUpdate();
+            } catch (error) {
+                console.error("Error renaming:", error);
+                // Handle error (e.g., show error message to user)
+            }
+
+            setIsRenaming(false);
+        } else {
+            setIsRenaming(true);
+        }
+    };
+
+    const handleDelete = async () => {
+        try {
+            if (node.isFolder) {
+                await indexedDBStore.deleteFolder(repoName, fullPath);
+            } else {
+                await indexedDBStore.deleteFile(repoName, fullPath);
+            }
+            onUpdate();
+        } catch (error) {
+            console.error("Error deleting:", error);
+            // Handle error (e.g., show error message to user)
+        }
+    };
+
+    const activeFolderClass = node.isFolder
+        ? isActive
+            ? "font-medium"
+            : isExpanded
+              ? "font-medium"
+              : "font-normal"
+        : "subtle";
+    const activeFileClass = !node.isFolder && selectedFile?.path === fullPath ? "bg-neutral-700" : "bg-neutral-0";
+
+    useEffect(() => {
+        setIsExpanded(node.isFolder && node.name === fullPath && selectedFile?.path.split("/").includes(fullPath));
+    }, [selectedFile]);
+
+    return (
+        <div {...others}>
+            <div className="flex items-center gap-2">
+                {isRenaming ? (
+                    <input
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                        onBlur={handleRename}
+                        autoFocus
+                    />
+                ) : (
+                    <button
+                        className={`flex-grow p-1 flex items-center gap-2 text-sm text-white rounded transition ease-in-out delay-150 border border-transparent hover:border-neutral-500
+                             ${activeFolderClass} ${activeFileClass}
+                             `}
+                        onClick={
+                            node.isFolder
+                                ? handleFolderClick
+                                : () => onFileSelect!(fullPath, decodeBase64(node?.content ?? ""))
+                        }
+                    >
+                        <FileIcon size={16} className={node.isFolder ? "text-yellow-400" : ""} />
+                        <span>{node.name}</span>
+                    </button>
+                )}
+                <Menu shadow="md" width={200}>
+                    <Menu.Target>
+                        <button className="">
+                            <IconDotsVertical size={16} />
+                        </button>
+                    </Menu.Target>
+                    <Menu.Dropdown>
+                        <Menu.Item leftSection={<IconPencil size={16} />} onClick={handleRename}>
+                            Rename
+                        </Menu.Item>
+                        <Menu.Item leftSection={<IconTrash size={16} />} onClick={handleDelete}>
+                            Delete
+                        </Menu.Item>
+                    </Menu.Dropdown>
+                </Menu>
+            </div>
+            {node.isFolder &&
+                isExpanded &&
+                node.children?.map((child) => (
+                    <TreeNode
+                        key={child.name}
+                        node={child}
+                        path={fullPath}
+                        repoName={repoName}
+                        onFileSelect={onFileSelect}
+                        onFolderSelect={onFolderSelect}
+                        onUpdate={onUpdate}
+                        activeFolder={activeFolder}
+                        style={{ marginLeft: 12 }}
+                        selectedFile={selectedFile}
+                    />
+                ))}
+        </div>
+    );
+};
--- app/dashboard/code-editor/components/Inputs/TextInput.tsx
+++ app/dashboard/code-editor/components/Inputs/TextInput.tsx
@@ -8,6 +8,7 @@ type TextInputProps = HTMLAttributes<HTMLInputElement> & {
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
     className?: string;
     icon?: ReactNode;
+    size?: "sm" | "md" | "lg";
 };
 
 export const TextInput: React.FC<TextInputProps> = ({
@@ -18,16 +19,26 @@ export const TextInput: React.FC<TextInputProps> = ({
     onChange,
     className = "",
     icon,
+    size = "md",
 }) => {
     const baseClass =
-        "px-2 py-1 border border-transparent bg-neutral-700 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-700 outline-none transition duration-150 ease-in-out placeholder:text-sm";
-    const inputClass = `${baseClass} ${className}`;
+        "border border-transparent bg-neutral-700 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-700 outline-none transition duration-150 ease-in-out placeholder:text-sm";
+
+    // Define size-specific classes
+    const sizeClass = {
+        sm: "px-2 py-1 text-sm", // Small size: smaller padding and text size
+        md: "px-3 py-2 text-base", // Medium size: default padding and text size
+        lg: "px-4 py-3 text-lg", // Large size: larger padding and text size
+    };
+
+    const inputClass = `${baseClass} ${sizeClass[size]} ${className}`;
 
     return (
         <div className={`flex flex-col ${className}`}>
             {label && <label className="mb-2 block text-sm font-medium leading-6 text-gray-900">{label}</label>}
             <div className="flex">
-                {icon}
+                {/* Render icon if provided */}
+                {icon && <span className="mr-2">{icon}</span>}
                 <input
                     type={type}
                     placeholder={placeholder}
